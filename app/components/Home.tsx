e2cbf00dd00144cd990189453a5362c20dea987d
hawkup <spiritsquare@gmail.com>
2020-03-22 16:47:31 +0700

    First integrate Xstate

--- app/components/Home.tsx
+++ app/components/Home.tsx
@@ -1,10 +1,19 @@
 import React from 'react';
+import { useMachine } from '@xstate/react';
+import toggleMachine from '../machines/toggle';
 import styles from './Home.css';
 
 export default function Home() {
+  const [state, send] = useMachine(toggleMachine);
+
   return (
     <div className={styles.container} data-tid="container">
       <h2>Home</h2>
+      <button type="button" onClick={() => send('TOGGLE')}>
+        {state.value === 'inactive'
+          ? 'Click to activate'
+          : 'Active! Click to deactivate'}
+      </button>
     </div>
   );
 }
